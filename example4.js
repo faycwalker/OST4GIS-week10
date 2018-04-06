1026e818b152f368b13f78e9b4e4d5f5d087e678
faycwalker <fay.c.walker@gmail.com>
2018-04-06 13:26:39 -0400

    walker_hw
    lab 1 and 2

---
+++ example4.js
@@ -0,0 +1,23 @@
+<button type="button" class="btn btn-danger" id= "modal" data-toggle="modal" data-target="#exampleModal">
+  Low Inventory
+</button>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tandy's Basket Weavers</h5>
+        <ul class="nav nav-pills">
+<li class="nav-item">
+  <a class="nav-link active" href="#">Edit</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Delete</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Export</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link disabled" href="#">Disabled</a>
+</li>
+</ul>
